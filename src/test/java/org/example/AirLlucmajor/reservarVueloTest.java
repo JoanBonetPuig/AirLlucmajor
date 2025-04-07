c4a6e620f2505aa389eb7fb87c199bc9c536b6d8
JoanBonetPuig <joanbonetpuig@gmail.com>
2025-04-07 19:09:22 +0200

    Add files via upload

---
+++ src/test/java/org/example/AirLlucmajor/reservarVueloTest.java
@@ -0,0 +1,14 @@
+package org.example.AirLlucmajor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class reservarVueloTest {
+
+    @org.junit.jupiter.api.Test
+    void reservarVuelo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReservas() {
+    }
+}
