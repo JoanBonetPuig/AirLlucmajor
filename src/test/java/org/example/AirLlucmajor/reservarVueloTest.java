b7eea2a82e874c2d690ed4e55fe6dcc97d752558
mbpCIFG1 <miquelbonetpuig@a.cifpperedesongall.org>
2025-04-08 10:41:42 +0200

    He creado los test unitarios de reservarVuelo

--- src/test/java/org/example/AirLlucmajor/reservarVueloTest.java
+++ src/test/java/org/example/AirLlucmajor/reservarVueloTest.java
@@ -1,14 +1,20 @@
 package org.example.AirLlucmajor;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class reservarVueloTest {
-
-    @org.junit.jupiter.api.Test
-    void reservarVuelo() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getReservas() {
+public class reservarVueloTest {
+    @Test
+    public void testSinVuelosDisponibles() {
+        ArrayList<añadirVuelosDisp> vuelos = new ArrayList<>();
+        String input = "CualquierNombre\n0\n";
+        System.setIn(new java.io.ByteArrayInputStream(input.getBytes()));
+        reservarVuelo.reservarVuelo(vuelos);
+        assertEquals(0, reservarVuelo.getReservas().size());
     }
 }
